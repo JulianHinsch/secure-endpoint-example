f507d06d0aab532a0741f72825292e6bfa9cda59
JulianHinsch <jhinsch799@gmail.com>
2018-12-02 17:59:23 -0500

    edited .gitignore

--- client/src/Auth/auth0-variables.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_CONFIG = {
-    domain: 'sneaker-stop.auth0.com',
-    clientId: 'p7POuzXkbMnJQ69LYg4ssaRac3j7AWad',
-    callbackUrl: 'http://localhost:3000/callback',
-    dbConnectionName: 'Username-Password-Authentication',
-}
